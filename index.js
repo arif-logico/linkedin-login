3c828ecd51a435421ca5c44eb17dc6f0170d111e
Habib1998 <Habib.kasbati@gmail.com>
2020-08-26 18:15:58 +0500

    Deploy on heroku

---
+++ index.js
@@ -0,0 +1,14 @@
+const PORT = 5000;
+const express = require('express');
+
+const app = express();
+
+const http = require('http');
+
+var server = http.Server(app);
+
+app.use(express.static('client/public'))
+
+server.listen(PORT, function() {
+    console.log('server is running')
+});
